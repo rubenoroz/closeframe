4389e1fcfb9ff4ff89820de39725b8639ce1e680
univa <univa@Ruben-Oroz-Mac-Studio.local>
2026-02-06 16:33:00 -0600

    feat: enforce social limits on public profile, remove pronouns feature, fix types

---
+++ types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import NextAuth, { DefaultSession } from "next-auth"
+import { UserRole } from "@prisma/client"
+
+declare module "next-auth" {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            role: UserRole
+        } & DefaultSession["user"]
+    }
+
+    interface User {
+        role: UserRole
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        role: UserRole
+    }
+}
